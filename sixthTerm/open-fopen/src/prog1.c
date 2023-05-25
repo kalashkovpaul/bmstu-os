f9c659311b853208d35cb4607b5944939bf60e82
Pavel Kalashkov <p.kalashkov@vk.team>
2023-05-25 19:30:21 +0300

    open and fopen done

--- sixthTerm/open-fopen/src/prog1.c
+++ sixthTerm/open-fopen/src/prog1.c
@@ -11,21 +11,21 @@ int main()
 
     FILE *fs2 = fdopen(fd,"r");
     char buff2[20];
-    setvbuf(fs2,buff2,_IOFBF,20);
-    
-    int flag1 = 1, flag2 = 2;
+    setvbuf(fs2, buff2,_IOFBF,20);
+
+    int flag1 = 1,  flag2 = 1;
     while(flag1 == 1 || flag2 == 1)
     {
         char c;
         flag1 = fscanf(fs1,"%c",&c);
-        if (flag1 == 1) 
+        if (flag1 == 1)
         {
             fprintf(stdout,"%c",c);
         }
         flag2 = fscanf(fs2,"%c",&c);
-        if (flag2 == 1) 
-        { 
-            fprintf(stdout,"%c",c); 
+        if (flag2 == 1)
+        {
+            fprintf(stdout,"%c",c);
         }
     }
     printf("\n");
